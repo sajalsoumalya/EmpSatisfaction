512fb26053e823f2a090b61a3effad869f4ef746
Soumalya Chowdhury <46195122+sajalsoumalya@users.noreply.github.com>
2022-08-17 15:42:54 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,4 @@
+import random
 import firebase_admin
 import streamlit as st
 
@@ -5,18 +6,19 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-
+r=random.randint(1,10000)
 cred = credentials.Certificate('./empsatisfaction.json')
-firebase_admin.initialize_app(cred,{'databaseURL': 'https://empsatisfaction-default-rtdb.firebaseio.com'})
-firebase_admin.get_app()
+App = firebase_admin.initialize_app(cred,
+{"apiKey": "AIzaSyDmjZZKURIk-ldqoPMZ6b5UXcxaP51qvuk",
+  "authDomain": "empsatisfaction.firebaseapp.com",
+  "projectId": "empsatisfaction",
+  "databaseURL": "https://empsatisfaction-default-rtdb.firebaseio.com",
+  "storageBucket": "empsatisfaction.appspot.com",
+  "messagingSenderId": "53038019314",
+  "appId": "1:53038019314:web:886437c48be901df85d66b"}, 
+name=str(r))
 
-"""try:
-    app = firebase_admin.get_app()
-except ValueError as e:
-    cred = credentials.Certificate('empsatisfaction.json')
-    firebase_admin.initialize_app(cred,{'databaseURL': 'https://empsatisfaction-default-rtdb.firebaseio.com'})
-    firebase_admin.get_app()"""
-ref = db.reference("py/")
+ref = db.reference("py")
 users_ref = ref.child('users')
 users_ref.set({
   'i':{
