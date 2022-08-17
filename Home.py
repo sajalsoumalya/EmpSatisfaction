c881196338c7443f996e7e755d386d0ce5cbae42
Soumalya Chowdhury <46195122+sajalsoumalya@users.noreply.github.com>
2022-08-17 17:23:54 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,38 +1,17 @@
 import random
-import firebase_admin
 import streamlit as st
-
-
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db
-r=random.randint(1,10000)
-cred = credentials.Certificate('./empsatisfaction.json')
-App = firebase_admin.initialize_app(cred,
-{"apiKey": "AIzaSyDmjZZKURIk-ldqoPMZ6b5UXcxaP51qvuk",
-  "authDomain": "empsatisfaction.firebaseapp.com",
-  "projectId": "empsatisfaction",
-  "databaseURL": "https://empsatisfaction-default-rtdb.firebaseio.com",
-  "storageBucket": "empsatisfaction.appspot.com",
-  "messagingSenderId": "53038019314",
-  "appId": "1:53038019314:web:886437c48be901df85d66b"}, 
-name=str(r))
+from firebase_admin import auth
 
-ref = db.reference("py")
-users_ref = ref.child('users')
-users_ref.set({
-  'i':{
-    'name':'SOUMALYA',
-    'DOB':'11'
+r=random.randint(1,10000)
+app_options = { 
+  'projectId': 'empsatisfaction',
+  'databaseURL': 'https://empsatisfaction-default-rtdb.firebaseio.com',
+  'storageBucket': 'empsatisfaction.appspot.com'
   }
-})
-"""
-  "apiKey": "AIzaSyDmjZZKURIk-ldqoPMZ6b5UXcxaP51qvuk",
-  "authDomain": "empsatisfaction.firebaseapp.com",
-  "projectId": "empsatisfaction",
-  "databaseURL": "https://empsatisfaction-default-rtdb.firebaseio.com",
-  "storageBucket": "empsatisfaction.appspot.com",
-  "messagingSenderId": "53038019314",
-  "appId": "1:53038019314:web:886437c48be901df85d66b"
-}
-"""
+
+cred = credentials.Certificate('./empsatisfaction.json')
+App = firebase_admin.initialize_app(cred,options=app_options, name=str(r))
+
+from firebase_admin import auth
