232f34c6a0b86474c34e3844af0ef6b77e169419
Soumalya Chowdhury <46195122+sajalsoumalya@users.noreply.github.com>
2022-08-17 12:27:23 +0530

    update

--- pages/Admin.py
+++ pages/Admin.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
-import streamlit_authenticator as stauth
 
 #from .Home import Experience  # pip install streamlit-authenticator
 
